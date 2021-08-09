8b4bd02ba49b70e9c9d6411ea7825f76a6b7b823
elligoro <elligoro@gmail.com>
2021-08-09 14:23:00 +0300

    1.createServiceProto continue

--- Services/CacheHandlerService.cs
+++ Services/CacheHandlerService.cs
@@ -1,16 +1,100 @@
 ﻿using Grpc.Core;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
 
 namespace loginAppRedis.Services
 {
     public class CacheHandlerService : CacheService.CacheServiceBase
     {
-        public override async Task<GetCacheResMessage> GetCache(GetCacheReqMessage request, ServerCallContext context)
+        private readonly IDistributedCache _cache;
+        private readonly ILogger _logger;
+        private readonly IConfiguration _config;
+        private readonly LoginLockPolicy _cacheConfig;
+
+        public CacheHandlerService(ILogger logger, IConfiguration config, IDistributedCache cache)
         {
-            
+            _cache = cache;
+            _logger = logger;
+            _config = config;
+            _cacheConfig = _config.GetValue<LoginLockPolicy>("LoginLockPolicy");
+        }
+        public override async Task<CacheResMessage> GetCache(CacheReqMessage request, ServerCallContext context)
+        {
+            try
+            {
+                var userTryCacheRes = await _cache.GetStringAsync(request.UserName);
+                var now = DateTime.UtcNow;
+                return new CacheResMessage { IsLocked = IsLocked(JsonConvert.DeserializeObject<CacheModel>(userTryCacheRes), _cacheConfig, now) };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"GetCache: error: {ex.Message}");
+                throw;
+            }
+        }
+
+        public override async Task<CacheResMessage> UpdateCache(CacheReqMessage request, ServerCallContext context)
+        {
+            try
+            {
+                var userTryCacheRes = await _cache.GetStringAsync(request.UserName);
+                var now = DateTime.UtcNow;
+                var model = UpdateCounter(JsonConvert.DeserializeObject<CacheModel>(userTryCacheRes), _cacheConfig, now);
+                await _cache.SetStringAsync(request.UserName, JsonConvert.SerializeObject(model));
+                return new CacheResMessage { IsLocked = IsLocked(JsonConvert.DeserializeObject<CacheModel>(userTryCacheRes), _cacheConfig, now) };
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"UpdateCache: error: {ex.Message}");
+                throw;
+            }
+        }
+
+        private bool IsLocked(CacheModel model, LoginLockPolicy cacheConfig, DateTime now) => !(model is null) && model.LockTimeStart.HasValue
+                                                                                                && model.LockTimeStart.Value.AddMinutes(cacheConfig.LockTime) > now;
+        private bool IsTryTimeSpan(CacheModel model, LoginLockPolicy cacheConfig, DateTime now) => !(model is null) && model.CountTimeStart.HasValue
+                                                                                                    && model.CountTimeStart.Value.AddMinutes(cacheConfig.TryTimeSpan) > now;
+        protected CacheModel UpdateCounter(CacheModel model, LoginLockPolicy cacheConfig, DateTime now)
+        {
+            var isLocked = IsLocked(model, cacheConfig, now);
+            var isTryTimeSpan = IsTryTimeSpan(model, cacheConfig, now);
+
+            if (!isLocked) // locked
+            {
+                if(!isTryTimeSpan) // if not locked and time span not up to date or not exists -> make new record
+                {
+                    model.Count = 1;
+                    model.CountTimeStart = now;
+                    model.LockTimeStart = null;
+                }
+                else
+                {
+                    model.Count++;
+                    if(model.Count >= cacheConfig.MaxTryLogin)
+                        model.LockTimeStart = now;
+                }
+            }
+
+            return model;
+          }        
+        }
+
+        public class CacheModel
+        {
+            public int Count { get; set; }
+            public DateTime? CountTimeStart { get; set; }
+            public DateTime? LockTimeStart { get; set; }
+        }
+        public class LoginLockPolicy
+        {
+                public int MaxTryLogin { get; set; }
+                public int LockTime { get; set; }
+                public int TryTimeSpan { get; set; }
         }
     }
-}
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using loginAppRedis.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -18,9 +20,12 @@ namespace loginAppRedis
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
+
+        public ILifetimeScope AutofacContainer { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -30,6 +35,24 @@ namespace loginAppRedis
             {
                 configuration.RootPath = "ClientApp";
             });
+            services.AddDistributedRedisCache(options => 
+            {
+                options.Configuration = Configuration
+                                        .GetConnectionString("RedisConnectionString");
+                options.InstanceName = "master";
+            });
+
+            services.AddGrpc(opt=> {
+                opt.EnableDetailedErrors = true;
+            });
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            /*
+                 services.AddScoped<IMyDependency, MyDependency>();
+                services.AddScoped<IMyDependency2, MyDependency2>();
+             */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +69,7 @@ namespace loginAppRedis
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGrpcService<CacheHandlerService>();
                 endpoints.MapControllers();
             });
 
