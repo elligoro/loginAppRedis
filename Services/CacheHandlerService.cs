e3fb11b69be8d219a0c2216247387a036199ceeb
elligoro <elligoro@gmail.com>
2021-08-09 10:30:30 +0300

    Add project files.

---
+++ Services/CacheHandlerService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace loginAppRedis.Services
+{
+    public class CacheHandlerService : CacheService.CacheServiceBase
+    {
+        public override async Task<GetCacheResMessage> GetCache(GetCacheReqMessage request, ServerCallContext context)
+        {
+            
+        }
+    }
+}
